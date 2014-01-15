9c7be46a4d41c424a0821ed552906d1651b29f67
MattDavis <davis.matthewv@gmail.com>
2014-01-15 17:03:31 -0500

    Robotium test case.

--- RobotiumHitMeButton/src/co/touchlab/HitMeButton/MyActivityTest.java
+++ RobotiumHitMeButton/src/co/touchlab/HitMeButton/MyActivityTest.java
@@ -1,6 +1,9 @@
 package co.touchlab.HitMeButton;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+import com.robotium.solo.Solo;
+import junit.framework.Assert;
 
 /**
  * This is a simple framework for a test of an Application.  See
@@ -12,12 +15,55 @@ import android.test.ActivityInstrumentationTestCase2;
  * -e class co.touchlab.HitMeButton.MyActivityTest \
  * co.touchlab.HitMeButton.tests/android.test.InstrumentationTestRunner
  */
-public class MyActivityTest extends ActivityInstrumentationTestCase2<MyActivity> {
+public class MyActivityTest extends ActivityInstrumentationTestCase2<MyActivity>
+{
+    private Solo solo;
 
-    public MyActivityTest() {
+    public MyActivityTest()
+    {
         super("co.touchlab.HitMeButton", MyActivity.class);
     }
 
+    /*
+     * Before each test, the Activity Under Test is re-launched, in this case HomeActivity
+     * setUp() gets called next, before each test begins.
+     */
+    public void setUp() throws Exception
+    {
+        solo = new Solo(getInstrumentation(), getActivity());
 
+        //Sanity Check - Ensure we're in the HomeActivity to start
+        solo.assertCurrentActivity("Not in MyActivity", "MyActivity", true);
+    }
 
+    public void testMyActivity() throws Exception
+    {
+        assertTrue(solo.searchButton("Hit Me!"));
+
+        solo.clickOnButton("Hit Me!");
+
+        assertTrue(solo.searchText("Ouch"));
+
+        solo.clickOnView(solo.getView(R.id.button));
+
+        assertFalse(solo.searchText("Ouch"));
+    }
+
+    /*
+     * tearDown() is called upon the completion of each test
+     */
+    @Override
+    public void tearDown() throws Exception
+    {
+        //Close all activities
+        solo.finishOpenedActivities();
+
+        try {
+            solo.finalize();
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+
+        super.tearDown();
+    }
 }
